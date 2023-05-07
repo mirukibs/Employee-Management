caf9cb2bbcf9df28c8e653e48c9d91fe1ef1de3d
Kibwana Miruru <99078034+mirukibs@users.noreply.github.com>
2023-05-07 22:47:30 +0300

    Initial commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.text.DecimalFormat;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Executive michael = new Executive(123, "Michael Sebastien", "CEO", 500000, 0, 0, 10000000, 0.35);
+
+        DecimalFormat decimalFormat = new DecimalFormat("##,###,###.00");
+        String formattedNumber = decimalFormat.format(michael.calculateSalary());
+
+        System.out.println(formattedNumber);
     }
 }
